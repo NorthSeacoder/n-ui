1304374d1b6982403e6566c852a13496de0050d8
mengpeng <646388689@qq.com>
2019-12-15 20:37:41 +0800

    [n-ui][upgrate] docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    base: '/n-ui/',
+    title: 'Nui',
+    description: 'Just playing around',
+    head: [['link', {rel: 'icon', href: '/n.png'}]],
+    markdown: {
+        lineNumbers: true
+    },
+    themeConfig: {
+        nav: [
+          { text: 'Home', link: '/' },
+        ]
+      }
+};
