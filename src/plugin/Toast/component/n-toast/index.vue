6ba820567a4f1f81e6205ab2df15cb7031413cee
mengpeng <646388689@qq.com>
2019-12-19 09:11:04 +0800

    [n-ui][toast] update css

--- src/plugin/Toast/component/n-toast/index.vue
+++ src/plugin/Toast/component/n-toast/index.vue
@@ -1,6 +1,18 @@
 <template>
-    <transition name="n-toast-fade">
+    <!-- <transition name="n-toast-fade">
         <div v-if="visible" :class="type" class="n-toast">{{ message }}</div>
+    </transition> -->
+    <transition name="n-toast-fade">
+        <div class="n-toast">
+            <div  class="alert  alert-simple alert-success alert-dismissible">
+                <button type="button" class="close font__size-18  disabled" data-dismiss="alert" v-if="showCross">
+                    <i class="greencross n-icon-close  fa-times"></i>
+                </button>
+                <i class="n-icon-success icon animated"></i>
+                <strong class="font__weight-semibold">Well done!</strong> You successfullyread this important.
+            </div>
+        </div>
+        
     </transition>
 </template>
 
@@ -11,6 +23,7 @@ export default {
     data() {
         return {
             visible: false,
+            showCross: false,
             message: '',
             type: ToastType.INFO
         };
