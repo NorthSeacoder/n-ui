82180eb865c9f13f9b2bfc55a1b40777a34c9a9d
mengpeng <pengmeng@yangqianguan.com>
2020-05-29 15:37:05 +0800

    opt plop

--- plop-templates/component/prompt.js
+++ plop-templates/component/prompt.js
@@ -1,4 +1,4 @@
-const { notEmpty, kebabCase, camelCase } = require("../utils.js");
+const { notEmpty, kebabCase, camelCase ,getNewMdActions,getNewSrcActions} = require("../utils.js");
 
 module.exports = {
   description: "generate a component",
@@ -15,15 +15,15 @@ module.exports = {
       message: "请选择类型",
       choices: [
         {
-          name: "指令",
+          name: "指令directive",
           value: "directive"
         },
         {
-          name: "组件",
+          name: "组件component",
           value: "component"
         },
         {
-          name: "插件",
+          name: "插件plugin",
           value: "plugin"
         }
       ]
@@ -34,102 +34,24 @@ module.exports = {
       message: "请选择组件类型",
       choices: [
         {
-          name: "基础类",
+          name: "基础类basic",
           value: "basic"
         },
         {
-          name: "表单类",
+          name: "表单类form",
           value: "form"
         },
         {
-          name: "插件类",
+          name: "插件类plugin",
           value: "plugin"
         }
       ]
     }
   ],
   actions: data => {
-    const kebabName = kebabCase(data.name);
-    const componentName = camelCase(data.name);
-    const compTypeReg=new RegExp(`//${data.compType}`);
-    const typeReg=new RegExp(`//${data.type}`);
+    const srcActions=getNewSrcActions(data);
+    const mdActions=getNewMdActions(data);
 
-    const actions = [
-      {
-        type: "add",
-        path: `src/${data.type}/${kebabName}/src/${kebabName}.vue`,
-        templateFile: "plop-templates/component/index.hbs",
-        data: {
-          componentName,
-          kebabName,
-          template: true,
-          script: true,
-          style: true
-        }
-      },
-      {
-        type: "add",
-        path: `src/${data.type}/${kebabName}/index.js`,
-        templateFile: "plop-templates/component/export.hbs",
-        data: {
-          componentName,
-          kebabName
-        }
-      },
-      {
-        type: "add",
-        path: `src/styles/${data.type}/${kebabName}.scss`,
-        templateFile: "plop-templates/component/css.hbs"
-      },
-      {
-        type: "add",
-        path: `docs/comp/${componentName}.md`,
-        templateFile: "plop-templates/component/md.hbs",
-        data: {
-          kebabName
-        }
-      },
-      {
-        type: "modify",
-        path: `docs/.vuepress/constant/componentsSidebar.js`,
-        pattern: compTypeReg,
-        template:'//{{compType}}\r\n\t\t\t\t"{{componentName}}",',
-        data: {
-          componentName,
-          compType: data.compType
-        }
-      },
-      {
-        type: "modify",
-        path: `src/${data.type}/index.js`,
-        pattern: typeReg,
-        template:'//{{type}}\r\n\t{{componentName}},',
-        data: {
-          componentName
-        }
-      },
-      {
-        type: "modify",
-        path: `src/${data.type}/index.js`,
-        pattern: new RegExp(`//import`),
-        template:"//import\r\nimport {{componentName}} from './{{kebabName}}';",
-        data: {
-          componentName
-        }
-      },
-      {
-        type: "modify",
-        path: `src/styles/${data.type}/index.scss`,
-        pattern: new RegExp(`//import`),
-        template:"//import\r\n@import './{{kebabName}}.scss';",
-        data: {
-          kebabName,
-          componentName
-        }
-      }
-
-    ];
-
-    return actions;
+    return [...srcActions,mdActions];
   }
 };
--- plop-templates/utils.js
+++ plop-templates/utils.js
@@ -1,24 +1,113 @@
-exports.notEmpty = name => {
-  return v => {
-    if (!v || v.trim === "") {
-      return `${name} 必须填写`;
-    }
-    return true;
-  };
+exports.notEmpty = (name) => {
+    return (v) => {
+        if (!v || v.trim === '') {
+            return `${name} 必须填写`;
+        }
+        return true;
+    };
 };
 
 // 下划线转换驼峰
 function camelCase(name) {
-  return name
-    .replace(/[\_|-](\w)/g, function(all, letter) {
-      return letter.toUpperCase();
-    })
-    .replace(/./, _ => _.toUpperCase());
+    return name
+        .replace(/[\_|-](\w)/g, function(all, letter) {
+            return letter.toUpperCase();
+        })
+        .replace(/./, (_) => _.toUpperCase());
 }
 // 驼峰转换中划线
 function kebabCase(name) {
-  return name.replace(/([A-Z])/g, "-$1").toLowerCase();
+    return name.replace(/([A-Z])/g, '-$1').toLowerCase();
 }
 
+function getNewSrcActions(data) {
+    const kebabName = kebabCase(data.name);
+    const componentName = camelCase(data.name);
+    const typeReg = new RegExp(`//${data.type}`);
+    return [
+        {
+            type: 'add',
+            path: `src/${data.type}/${kebabName}/src/${kebabName}.vue`,
+            templateFile: 'plop-templates/component/index.hbs',
+            data: {
+                componentName,
+                kebabName,
+                template: true,
+                script: true,
+                style: true,
+            },
+        },
+        {
+            type: 'add',
+            path: `src/${data.type}/${kebabName}/index.js`,
+            templateFile: 'plop-templates/component/export.hbs',
+            data: {
+                componentName,
+                kebabName,
+            },
+        },
+        {
+            type: 'add',
+            path: `src/styles/${data.type}/${kebabName}.scss`,
+            templateFile: 'plop-templates/component/css.hbs',
+        },
+        {
+            type: 'modify',
+            path: `src/${data.type}/index.js`,
+            pattern: typeReg,
+            template: '//{{type}}\r\n\t{{componentName}},',
+            data: {
+                componentName,
+            },
+        },
+        {
+            type: 'modify',
+            path: `src/${data.type}/index.js`,
+            pattern: new RegExp(`//import`),
+            template: "//import\r\nimport {{componentName}} from './{{kebabName}}';",
+            data: {
+                componentName,
+                kebabName,
+            },
+        },
+        {
+            type: 'modify',
+            path: `src/styles/${data.type}/index.scss`,
+            pattern: new RegExp(`//import`),
+            template: "//import\r\n@import './{{kebabName}}.scss';",
+            data: {
+                kebabName,
+                componentName,
+            },
+        },
+    ];
+}
+function getNewMdActions(data) {
+    const kebabName = kebabCase(data.name);
+    const compTypeReg = new RegExp(`//${data.compType}`);
+    return [
+        {
+            type: 'add',
+            path: `docs/comp/${kebabName}.md`,
+            templateFile: 'plop-templates/component/md.hbs',
+            data: {
+                kebabName,
+            },
+        },
+        {
+            type: 'modify',
+            path: `docs/.vuepress/constant/componentsSidebar.js`,
+            pattern: compTypeReg,
+            template: '//{{compType}}\r\n\t\t\t\t"{{kebabName}}",',
+            data: {
+                kebabName,
+                compType: data.compType,
+            },
+        },
+    ];
+}
+
+exports.getNewMdActions = getNewMdActions;
+exports.getNewSrcActions = getNewSrcActions;
 exports.camelCase = camelCase;
 exports.kebabCase = kebabCase;
